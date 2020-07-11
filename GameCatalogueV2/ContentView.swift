e957eb1828b2ca9b68a49a699040447ee3b7e796
Muhammad Hendra <mhpranajaya@gmail.com>
2020-07-11 15:28:14 +0700

    first init

---
+++ GameCatalogueV2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GameCatalogueV2
+//
+//  Created by Daniel Manullang on 11/07/20.
+//  Copyright © 2020 Muhammad Hendra Pranajaya. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
